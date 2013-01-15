e82a3dfc70c6aff90606561582cbec41f4fc8899
José Padilla <jpadilla@webapplicate.com>
2013-01-15 07:29:53 -0400

    Update setup.py
    Add download_url for pypi

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     author_email='jpadilla@getblimp.com',
     packages=find_packages(),
     url='https://github.com/getblimp/blimp-python/',
+    download_url='https://github.com/GetBlimp/blimp-python/tarball/master',
     license='MIT License',
     description='Blimp library for Python',
     long_description='Blimp library for Python',
